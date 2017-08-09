647fc53f7b93e0aae2a57b71a1fcca1dcc97026d
reid <reidsherman@gmail.com>
2017-08-09 12:04:41 -0400

    started directedcycle algos

--- directedcycle.js
+++ directedcycle.js
@@ -28,7 +28,7 @@ function Vertex(num) {
   this.num = num
   this.marked = false
   this.adj = [] //adjacent vertices
-  this.cycle = null
+  this.cycles = []
   this.connect = function(vertex) {
     if (vertex === undefined)
       return false;
@@ -37,27 +37,25 @@ function Vertex(num) {
   //TO DO: Implement directed cycle search
   //check all nodes to see if there is a cycle
   this.directedCycle = function() {
-
+    directedDFSToFindCycle
   }
   //check if the target is reachable by depth first search
-  this.directedDFSToFindCycle = function(target) {
-    //mark this and then mark adjacent vertices
+  this.directedDFSToFindCycle = function() {
     this.marked = true
     //check all adjacent vertices
     for (i in this.adj) {
       p("checking vertex: ", this.adj[i].num)
-      if (this.adj[i] === target) {
-        p(String(target.num), "is reachable")
-        return true
+      if (this.adj[i].marked) {
+        p("here")
       }
       if (!this.adj[i].marked) {
         //then search all of their adjacent
-        this.adj[i].directedDFS(target)
+        this.adj[i].directedDFSToFindCycle()
       }
     }
   }
   this.hasCycle = function() {
-    return (this.cycle !== null)
+    return (this.cycles.length !== 0)
   }
 }
 p = console.log
@@ -71,5 +69,16 @@ zero.connect(five)
 five.connect(four)
 four.connect(three)
 three.connect(five)
-
+zero.directedCycle()
 AssertEqual(zero.hasCycle(), true, "The zero vertex should be part of a cycle with the other vertices")
+zero = new Vertex(0)
+one = new Vertex(1)
+five = new Vertex(5)
+four = new Vertex(4)
+three = new Vertex(3)
+zero.connect(one)
+zero.connect(five)
+five.connect(four)
+four.connect(three)
+zero.directedCycle()
+AssertEqual(zero.hasCycle(), false, "The zero vertex should be part of a cycle with the other vertices")
