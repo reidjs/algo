ba10a9abf188e022d4b08577fbebeaadfcc0146b
reid <reidsherman@gmail.com>
2017-08-11 17:09:33 -0400

    fixed critical error with the dfs function. it wasn't looping correctly

--- vertexorder.js
+++ vertexorder.js
@@ -36,21 +36,61 @@ function Graph() {
   this.connectDirectionally = function(v1, v2) {
     vertices[v1].adj.push(v2)
   }
+  this.returnVerticesAsArray = function(){
+    return Object.keys(vertices)
+  }
+  this.returnFirstVerticeAsObject = function() {
+    return this.v(Object.keys(vertices)[0])
+  }
+  this.returnFirstVerticeAsNumber = function(){
+    return Object.keys(vertices)[0]
+  }
 }
 
-//pass the graph and starting vertex into the depth first search function
-//will run the function func at each vertex
+//pass the graph and starting vertex NUMBER!! (not the vertex itself)
 //OPTIONAL: if you include a function as the third var, it will call
-//it every pass with the graph and vertex as params
-function dfs(g, v, func = function(){}) {
+//before every pass with the graph and vertex as params
+//OPTIONAL: If you include a function as fourht var, it will call
+//AFTER every pass with grpah and vertex as params
+function dfs(g, v, pre_func = function(){}, post_func = function(){}) {
   vertex = g.v(v)
-  func.apply(this, [g,vertex])
+  pre_func.apply(this, [g,vertex])
   vertex.marked = true
-  for(i in vertex.adj) {
+  for(i = 0; i < vertex.adj.length; i++) {
+    //PROBLEM: doesn't work if nodes are two directional
+    p("current", vertex)
     next_vertex = g.v(vertex.adj[i])
+    p("next", next_vertex)
     if (!next_vertex.marked)
-      dfs(g, next_vertex.num, func)
+        dfs(g, next_vertex.num, pre_func, post_func)
   }
+  post_func.apply(this, [g,vertex])
+}
+
+function vertexOrder(g) {
+  //the graph MUST not have a directed cycle to topologically sort.
+  if (hasDirectedCycle(g))
+    return false
+  var pre = [] //integer queue
+  var post = [] //integer queue
+  var reversePost = [] //integer stack
+  this.preEnqueue = function(g, v){
+    pre.unshift(v)
+  }
+  this.postEnqueue = function(g, v){
+    post.unshift(v)
+    reversePost.push(v)
+  }
+  dfs(g, g.returnFirstVerticeAsNumber(), this.preEnqueue, this.postEnqueue)
+  p(reversePost)
+}
+//starting at a random vertice in the graph g, returns TRUE if there is a
+//cycle and returns FALSE if there is not a cycle.
+function hasDirectedCycle(g) {
+  start_vertice = g.returnVerticesAsArray()[0]
+  return findDirectedCycle(g, start_vertice)
+  //p(Object.keys(g.v))
+  //findDirectedCycle(g, g.)
 }
 
 //Returns TRUE if there is a cycle, FALSE if not
@@ -101,13 +141,23 @@ function Vertex(num) {
 p = console.log
 
 g = new Graph()
+g.addVertex(0)
 g.addVertex(1)
 g.addVertex(2)
 g.addVertex(3)
-g.connectDirectionally(1,2)
+g.addVertex(5)
+g.connectDirectionally(0,1)
+g.connectDirectionally(1,0)
+g.connectDirectionally(0,5)
 g.connectDirectionally(2,3)
-g.connectDirectionally(3,1)
-p(findDirectedCycle(g, 1))
+g.connectDirectionally(2,0)
+g.connectDirectionally(3,5)
+g.connectDirectionally(3,2)
+//g.connectDirectionally(3,1)
+//p(findDirectedCycle(g, 1))
+p("Directed cycle:", hasDirectedCycle(g))
+vertexOrder(g)
+
 //dfs(g, 1, printVertexName)
 //dfs(g, 1)
 // p(g.checkVertex(2))
